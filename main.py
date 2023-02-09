1852741fbc739d32876ffdacecb42343b9c7ed95
MarcosSantanaAQ <marcos.santana@aquiris.com.br>
2023-02-08 23:26:12 -0300

    planning

---
+++ main.py
@@ -0,0 +1,14 @@
+# Bot that will allow you to change your permissions based on reaction in a message
+
+# (1) Check if message exists, if not creates a message and adds reactions to it //
+# if it exists ignore //
+# Also check if the message has all the expected and remove or add them if necessary //
+# reactions should be in a dictionary that can be edited to add or remove new reactions options //
+# find a way to get a role id based on the name //
+# The message should use embed //
+
+# (2) listen to an event related to adding or removing a reaction from that message //
+# if it is added, add the equivalent permission to the User //
+# if it is removed, remove the Permission //
+
+# (3) allows the user to use a command to check all the permissions he has //
